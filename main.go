906824ea38648d63610f15e7d60653ef534e034f
Rudi Strydom <iam@thatguy.co.za>
2015-08-16 21:56:36 +0200

    Adding signal listeners

--- main.go
+++ main.go
@@ -1,21 +1,41 @@
 package main
 
 import (
+	"flag"
 	"github.com/justinas/alice"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
-	"flag"
 )
 
 var dir = flag.String("directory", "public/", "directory of web files")
+
 func main() {
 
+	// Os specifics. Signal handlers and flag parser
 	flag.Parse()
+	sigs := make(chan os.Signal, 1)
+	done := make(chan bool, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		sig := <-sigs
+		log.Println(sig)
+		done <- true
+	}()
+
+	hostPort := "127.0.0.1:8080"
+	log.Printf("goServe Started. Listening on %s", hostPort)
+
 	requestHandlers := alice.New(loggingHandler, recoverHandler)
 	http.Handle("/", requestHandlers.ThenFunc(indexHandler))
 	http.Handle("/favicon.ico", requestHandlers.ThenFunc(favIcoHandler))
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(hostPort, nil)
+
+	<-done
+	log.Println("goServe exiting")
 }
 
 func indexHandler(rw http.ResponseWriter, req *http.Request) {
