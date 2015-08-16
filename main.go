8ab86a453d31fd7ee0b7e2daa8c4ec567170e7ac
Rudi Strydom <iam@thatguy.co.za>
2015-08-16 22:11:54 +0200

    Nope nope nope

--- main.go
+++ main.go
@@ -5,9 +5,6 @@ import (
 	"github.com/justinas/alice"
 	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
 	"time"
 )
 
@@ -15,34 +12,21 @@ var dir = flag.String("directory", "public/", "directory of web files")
 
 func main() {
 
-	// Os specifics. Signal handlers and flag parser
 	flag.Parse()
-	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		sig := <-sigs
-		log.Println(sig)
-		done <- true
-	}()
-
-	hostPort := "127.0.0.1:8080"
-	log.Printf("goServe Started. Listening on %s", hostPort)
-
+	log.Println("goServe Started")
 	requestHandlers := alice.New(loggingHandler, recoverHandler)
 	http.Handle("/", requestHandlers.ThenFunc(indexHandler))
 	http.Handle("/favicon.ico", requestHandlers.ThenFunc(favIcoHandler))
-	http.ListenAndServe(hostPort, nil)
-
-	<-done
-	log.Println("goServe exiting")
+	http.ListenAndServe(":8080", nil)
 }
 
 func indexHandler(rw http.ResponseWriter, req *http.Request) {
+	log.Println("Index handler")
 	http.ServeFile(rw, req, "public/index.html")
 }
 
 func favIcoHandler(rw http.ResponseWriter, req *http.Request) {
+	log.Println("Fav handler")
 	http.ServeFile(rw, req, "public/favicon.ico")
 }
 
