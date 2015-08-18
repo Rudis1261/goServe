ab839a8a875c7664b528be968976babf91c78427
Rudi Strydom <iam@thatguy.co.za>
2015-08-18 14:13:30 +0200

    Docker Example

--- main.go
+++ main.go
@@ -5,28 +5,43 @@ import (
 	"github.com/justinas/alice"
 	"log"
 	"net/http"
+	"os"
+	//"path/filepath"
 	"time"
 )
 
-//var dir = flag.String("directory", "public/", "directory of web files")
+var PATH = flag.String("path", "public", "directory of web files")
 var PORT = flag.String("port", "3000", "On which port the HTTP server listens")
 
 func main() {
 
 	flag.Parse()
 	log.Printf("goServe Started, listening on PORT: %s", *PORT)
-	requestHandlers := alice.New(loggingHandler, recoverHandler)
-	http.Handle("/", requestHandlers.ThenFunc(indexHandler))
+	requestHandlers := alice.New(loggingHandler, recoverHandler, imgHandler)
 	http.Handle("/favicon.ico", requestHandlers.ThenFunc(favIcoHandler))
-	http.ListenAndServe(":" + *PORT, nil)
+	http.Handle("/", requestHandlers.ThenFunc(indexHandler))
+	http.ListenAndServe(":"+*PORT, nil)
 }
 
 func indexHandler(rw http.ResponseWriter, req *http.Request) {
-	http.ServeFile(rw, req, "public/index.html")
+	http.ServeFile(rw, req, *PATH+"/index.html")
 }
 
 func favIcoHandler(rw http.ResponseWriter, req *http.Request) {
-	http.ServeFile(rw, req, "public/favicon.ico")
+	http.ServeFile(rw, req, *PATH+"/favicon.ico")
+}
+
+func imgHandler(next http.Handler) http.Handler {
+	fn := func(rw http.ResponseWriter, req *http.Request) {
+		reqUrl := req.URL.String()
+		if _, err := os.Stat(*PATH + reqUrl); os.IsNotExist(err) {
+			next.ServeHTTP(rw, req)
+		} else {
+			http.ServeFile(rw, req, *PATH+reqUrl)
+		}
+	}
+
+	return http.HandlerFunc(fn)
 }
 
 func loggingHandler(next http.Handler) http.Handler {
